a6c3c07239164ae17bba73f52940e92d389b6276
kolommi1 <kolommi1@uhk.cz>
2019-02-18 16:22:53 +0100

    Detail Activity, Toast popups, Předávání dat mezi aktivitami

--- app/src/main/java/cz/uhk/myapplication/MainActivity.kt
+++ app/src/main/java/cz/uhk/myapplication/MainActivity.kt
@@ -1,12 +1,33 @@
 package cz.uhk.myapplication
 
+import android.app.Activity
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        openButton.setOnClickListener {
+            val intent = Intent(this,DetailActivity::class.java)
+            intent.putExtra("key","text")
+            startActivityForResult(intent,777)
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(requestCode == 777 && resultCode == Activity.RESULT_OK){
+            val s = data?.getStringExtra("result")
+            //var - proměnná
+            //val - value - jen pro čtení
+            Toast.makeText(this,s,Toast.LENGTH_SHORT).show()
+        }
     }
 }
